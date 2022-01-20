51dc93b0caaec0221d890687f2737a8335f20f40
sardar112 <sardarsaqib385@gmail.com>
2022-01-20 22:03:21 +0500

    GraphQL APIs completed

--- utils/util.js
+++ utils/util.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports.connection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
+    await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
